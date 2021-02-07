d0b98360d2138543f2abb32f59b2f613c7c45019
wangfei <wangfei>
2021-02-07 23:49:19 +0800

    build project

--- src/main/java/com/generate/start/GenerateCodeStart.java
+++ src/main/java/com/generate/start/GenerateCodeStart.java
@@ -2,13 +2,15 @@ package com.generate.start;
 
 
 import com.generate.build.*;
+import com.generate.service.ExecuteService;
 
 import java.util.Map;
 
 
 public class GenerateCodeStart {
 
-    public static void code(String path, Map<String, Object> data) {
+    public static void code(String path, Class<?> clazz) {
+        Map<String, Object> data = ExecuteService.getClassInfo(clazz);
         Controller.write(path, data);
         Dao.write(path, data);
         Service.write(path, data);
